ca6a72450712cd19c55fab889f3d7fa9681fe577
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-03-12 11:35:26 +0100

    quick update

--- src/main/java/DBAccess/OrderMapper.java
+++ src/main/java/DBAccess/OrderMapper.java
@@ -12,7 +12,23 @@ public class OrderMapper {
     public static void makeOrder(Order order) throws LoginSampleException {
         try {
             Connection con = Connector.connection();
-            String SQL = "INSERT INTO ";
+            String SQL = "INSERT INTO orders (user, top, bot, amount, sum) values (?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement( SQL);
+            ps.setString(1, "placeholder");
+            ps.executeUpdate();
+            ResultSet ids = ps.getGeneratedKeys();
+        } catch ( SQLException | ClassNotFoundException ex ) {
+            throw new LoginSampleException( ex.getMessage() );
+        }
+    }
+
+    public static void getPrices(Order order) throws LoginSampleException {
+        String top = order.getTop();
+        String bot = order.getBot();
+        int sum = order.getSum();
+        try {
+            Connection con = Connector.connection();
+            String SQL = "Select";
             PreparedStatement ps = con.prepareStatement( SQL);
             ps.setString(1, "placeholder");
             ps.executeUpdate();
