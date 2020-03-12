de291023a4e3c44da41adbcafb0bd61498c921ca
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-03-12 11:48:41 +0100

    ordercommand

---
+++ src/main/java/PresentationLayer/OrderCommand.java
@@ -0,0 +1,16 @@
+package PresentationLayer;
+
+import FunctionLayer.LoginSampleException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OrderCommand extends Command {
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+
+
+
+        return null;
+    }
+}
