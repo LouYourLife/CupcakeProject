d0b5f0ede71945a5f307a47c5c4861eab368ebce
MattiBSH <cph-mh829@cphbusiness.dk>
2020-03-16 12:34:41 +0100

    bum

--- src/main/java/TestJava.java
+++ src/main/java/TestJava.java
@@ -5,9 +5,6 @@ import FunctionLayer.User;
 
 public class TestJava {
     public static void main(String[] args) throws LoginSampleException {
-      //  String username, String top, String bot, int amount, int sum, int id
-        Order e = new Order("jens@somewhere.com","Crispy","Chocolate",7, 77, 1);
-
-        System.out.println(OrderMapper.findID(e));
+      
     }
 }
