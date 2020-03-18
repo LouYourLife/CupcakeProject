5550a8c8df5c2b9b20e56508229ffb6e277976f6
Sjuber <54933648+Sjuber@users.noreply.github.com>
2020-03-18 14:23:12 +0100

    Dynamisk tilføjelse og slettelse af ordre

--- src/main/java/PresentationLayer/DeleteOrder.java
+++ src/main/java/PresentationLayer/DeleteOrder.java
@@ -2,13 +2,14 @@ package PresentationLayer;
 
 import DBAccess.OrderMapper;
 import FunctionLayer.LoginSampleException;
+import FunctionLayer.Order;
 import FunctionLayer.OrderHelper;
 import FunctionLayer.User;
 
-import javax.persistence.criteria.Order;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 public class DeleteOrder extends Command {
     @Override
@@ -23,7 +24,8 @@ public class DeleteOrder extends Command {
 // Hvis useren på sessionen er den samme som den på orderen, så kan du godt delende est carthago AKA kan vi godt delete
 
      //  OrderMapper.deleteOrder(o);
-
+        ArrayList<Order> userOrders = OrderMapper.seeOrders2((String) session.getAttribute("email"));
+        session.setAttribute("userOrders", userOrders);
         return "CartPage";
     }
 }
