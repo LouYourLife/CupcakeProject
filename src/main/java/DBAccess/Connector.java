4cb21552e38b27ae9ed4afecc01f915c32b148e2
MattiBSH <cph-mh829@cphbusiness.dk>
2020-03-23 14:39:48 +0100

    connector

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -11,9 +11,9 @@ import java.sql.SQLException;
  */
 public class Connector {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/cakeorders?serverTimezone=CET&useSSL=false";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "cph39178";
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
 
 
     private static Connection singleton;
@@ -24,10 +24,27 @@ public class Connector {
 
     public static Connection connection() throws ClassNotFoundException, SQLException {
         if ( singleton == null ) {
+            setDBCredentials();
             Class.forName( "com.mysql.cj.jdbc.Driver" );
             singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
         }
         return singleton;
     }
+
+    private static void setDBCredentials() {
+        String deployed = System.getenv("DEPLOYED");
+        if(deployed!=null){
+            URL=System.getenv("JDBC_CONNECTION_STRING");
+            USERNAME=System.getenv("JDBC_USER");
+            PASSWORD=System.getenv("JDBC_PASSWORD");
+
+        }else {
+            URL="jdbc:mysql://localhost:3306/cakeorders?serverTimezone=CET&useSSL=false";
+            USERNAME="root";
+            PASSWORD="Matti12345h";
+        }
+    }
+
+
 
 }
