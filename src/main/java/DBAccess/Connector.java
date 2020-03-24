9ad1c297a41de41d8ac11800eb3d1f6a7935b809
LouYourLife <vibekenordestgaard@gmail.com>
2020-03-24 12:15:30 +0100

    header

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -13,7 +13,7 @@ public class Connector {
 
     private static final String URL = "jdbc:mysql://localhost:3306/cakeorders?serverTimezone=CET&useSSL=false";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "cph39178";
+    private static final String PASSWORD = "";
 
 
     private static Connection singleton;
